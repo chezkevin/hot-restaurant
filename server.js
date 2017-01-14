1072c852617045802fc6dcac900d43bf798960ac
Josh Bruggeman <joshrbruggeman@gmail.com>
2017-01-14 10:47:55 -0600

    first updates

--- server.js
+++ server.js
@@ -23,6 +23,13 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
